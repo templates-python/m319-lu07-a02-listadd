965a780d36e07ea39bd94cc13f37579c23133353
Marcel Suter <marcel.suter@bzz.ch>
2023-10-02 14:15:04 +0200

    updated tests

--- main_test.py
+++ main_test.py
@@ -1,12 +1,33 @@
 import main
 
+
 def test_while_existing(capsys, monkeypatch):
-    assert True
+    inputs = iter(['pineapple'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.while_else()
+    output = capsys.readouterr().out
+    assert 'Frucht bereits vorhanden' in output
+
 
 def test_while_new(capsys, monkeypatch):
-    assert True
+    inputs = iter(['apple'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.while_else()
+    output = capsys.readouterr().out
+    assert output == "Inhalt der Liste: ['papaya', 'banana', 'pineapple', 'mango', 'grapes', 'apple']\n"
+
 
 def test_in_existing(capsys, monkeypatch):
-    assert True
+    inputs = iter(['mango'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.if_in()
+    output = capsys.readouterr().out
+    assert 'Frucht bereits vorhanden' in output
+
+
 def test_in_new(capsys, monkeypatch):
-    assert True
+    inputs = iter(['pear'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main.if_in()
+    output = capsys.readouterr().out
+    assert output == "Inhalt der Liste: ['papaya', 'banana', 'pineapple', 'mango', 'grapes', 'pear']\n"
