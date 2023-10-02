f2e6648c8aec851b1d7740e3d2b03dd9a820a91e
Marcel Suter <marcel.suter@bzz.ch>
2023-10-02 14:04:53 +0200

    updates to workflows and template

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+def while_else():
+    # list of fruits
+    my_list = ['papaya', 'banana', 'pineapple', 'mango', 'grapes']
+    new_fruit = input('Welche Frucht möchten Sie der Liste ergänzen?\n')
 
+
+    #TODO: Iterieren Sie über die Liste der Früchte um zu überprüfen ob new_fruit bereits in der Liste ist,
+    # ist die Frucht new_fruit vorhanden, geben Sie aus 'Frucht bereits vorhanden'
+    # ist new_fruit nicht vorhanden, so fügen Sie diese der Liste hinzu.
+    # Verwenden Sie dazu eine while-else-Anweisung
+
+
+    print(f'Inhalt der Liste: {my_list}')
+
+def if_in():
+    # list of fruits
+    my_list = ['papaya', 'banana', 'pineapple', 'mango', 'grapes']
+    new_fruit = input('Welche Frucht möchten Sie der Liste ergänzen?\n')
+
+    # TODO: Prüfen Sie mit if-in ob die Frucht new_fruit schon der Liste my_list existiert.
+    # ist die Frucht new_fruit vorhanden, geben Sie aus 'Frucht bereits vorhanden'
+    # ist new_fruit nicht vorhanden, so fügen Sie diese der Liste hinzu.
+
+    print(f'Inhalt der Liste: {my_list}')
 
 if __name__ == '__main__':
-    calculate()
+    while_else()
+    if_in()
--- main_test.py
+++ main_test.py
@@ -1,6 +1,12 @@
 import main
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_while_existing(capsys, monkeypatch):
+    assert True
+
+def test_while_new(capsys, monkeypatch):
+    assert True
+
+def test_in_existing(capsys, monkeypatch):
+    assert True
+def test_in_new(capsys, monkeypatch):
+    assert True
